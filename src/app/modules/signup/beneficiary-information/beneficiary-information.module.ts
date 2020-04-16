ce4ad776a6e7ee027c852d5226a6570d88331e27
raihan zaman <raihanuzzaman@bs-23.net>
2020-04-16 16:12:24 +0600

    added facade in module

--- src/app/modules/signup/beneficiary-information/beneficiary-information.module.ts
+++ src/app/modules/signup/beneficiary-information/beneficiary-information.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { BeneficiaryInformationPage } from './container/beneficiary-information.page';
 import { SignupComponentsModule } from '../components';
 import { SharedModule } from '@app/shared';
-
+import { FACADE_SERVICE } from './facade';
 const routes: Routes = [
   {
     path: '',
@@ -13,6 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forChild(routes), SharedModule, SignupComponentsModule],
+  providers: [FACADE_SERVICE],
   declarations: [BeneficiaryInformationPage]
 })
 export class BeneficiaryInformationPageModule {}
