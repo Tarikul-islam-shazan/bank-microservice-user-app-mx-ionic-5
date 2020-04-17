65454de9b6db732e8388c38a3b91fa3ab3786fb8
Zahidul Islam <zahidul@bs-23.net>
2020-04-17 14:22:33 +0600

    form group init

--- src/app/modules/signup/general-information/container/general-information.page.ts
+++ src/app/modules/signup/general-information/container/general-information.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mbc-general-information',
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./general-information.page.scss']
 })
 export class GeneralInformationPage implements OnInit {
-  constructor() {}
+  generalForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.initGeneralInfoForm();
+  }
+
+  initGeneralInfoForm() {
+    this.generalForm = this.formBuilder.group({
+      firstName: [null, [Validators.required, Validators.maxLength(26)]],
+      secondName: [null, [Validators.maxLength(26)]],
+      paternalLastName: [null, [Validators.required, Validators.maxLength(26)]],
+      maternalLastName: [null, [Validators.maxLength(26)]],
+      dateOfBirth: [null, [Validators.required]],
+      curp: [null, [Validators.required, Validators.maxLength(18)]],
+      mobileNumber: [null, [Validators.required, Validators.maxLength(10)]]
+    });
+  }
 }
--- src/app/modules/signup/general-information/general-information.module.ts
+++ src/app/modules/signup/general-information/general-information.module.ts
@@ -6,9 +6,11 @@ import { GeneralInformationPage } from './container/general-information.page';
 
 import { SignupComponentsModule } from '../components';
 import { SharedModule } from '@app/shared';
+import { FACADE_SERVICE } from './facade';
 
 @NgModule({
   imports: [SharedModule, SignupComponentsModule, GeneralInformationPageRoutingModule],
+  providers: [FACADE_SERVICE],
   declarations: [GeneralInformationPage]
 })
 export class GeneralInformationPageModule {}
