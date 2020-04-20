d50d69225c87dabbefee8f114d0e2a8ac8e14e8a
Tarikul Islam <kmtarikulislamshazan14062@gmail.com>
2020-04-20 17:28:18 +0600

    Refactor input decimal mask directive.

--- src/app/modules/shared/directives/input-decimal-mask.directive.ts
+++ src/app/modules/shared/directives/input-decimal-mask.directive.ts
@@ -1,137 +1,138 @@
-import { Directive, HostListener, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 import { IonInput } from '@ionic/angular';
+import { SettingsService } from '@app/core/services/settings.service';
+import { REG_EX_PATTERNS } from '@app/core/models/patterns';
+interface IInputMaskDecimal {
+  symbol?: string;
+  noOfDecimalPoint?: number;
+  maxDigit?: number;
+  decModel?: string;
+}
 @Directive({
   selector: '[appInputDecimalMask]'
 })
-export class InputDecimalMaskDirective implements OnInit {
-  inputRef: IonInput;
-  value: string;
-  noOfDecimal = 2;
-  maxDigit = 15;
-  flag = false;
-  hideCurrency = false;
-  currency = '$';
-
+export class InputDecimalMaskDirective {
   @Input() set decModel(val: string) {
-    this.value = val;
-    this.flag = true;
-    this.ngOnInit();
+    if (val) {
+      this.inputRef.value = val;
+      this.setDecimalMask({ decModel: val });
+      this.initTemplateValue();
+    }
   }
-  @Input() set 'no-of-decimal'(val) {
-    this.noOfDecimal = val;
-  }
-
-  @Input() set 'hide-currency'(val) {
-    this.currency = '';
-    this.hideCurrency = true;
-    this.ngOnInit();
-  }
-  @Output() decModelChange = new EventEmitter();
-
-  constructor(inputRef: IonInput) {
-    this.inputRef = inputRef;
-  }
-  ngOnInit() {
-    setTimeout(() => {
-      if (this.flag) {
-        this.inputRef.value = this.value;
-      }
-      if (this.inputRef.value) {
-        let val = this.inputRef.value.toString();
-        val = val.replace(/[^0-9.]/g, '');
-        if (val.indexOf('.') < 0) {
-          val = parseFloat(val)
-            .toFixed(this.noOfDecimal)
-            .toString();
-        } else {
-          const a = val.split('.');
-          if (a[1].length > this.noOfDecimal) {
-            val = val.slice(0, val.indexOf('.') + 1 + this.noOfDecimal);
-          } else {
-            val = parseFloat(val)
-              .toFixed(this.noOfDecimal)
-              .toString();
-          }
-        }
-        val = val.replace(/[^0-9]/g, '');
-        this.inputRef.value = this.setDecimal(val.toString(), this.noOfDecimal);
-      }
+  decimalMask: IInputMaskDecimal;
+  constructor(private inputRef: IonInput, private settingsService: SettingsService) {
+    this.setDecimalMask({
+      symbol: this.settingsService.getCurrencySymbol,
+      noOfDecimalPoint: 2,
+      maxDigit: 15
     });
   }
 
-  @HostListener('ionFocus', ['$event']) onfocus(e) {
+  setDecimalMask(inputMaskDecimal: IInputMaskDecimal) {
+    this.decimalMask = { ...this.decimalMask, ...inputMaskDecimal };
+  }
+
+  /**
+   * Its just format input value which is come from template @input
+   *
+   * @private
+   * @memberof InputDecimalMaskDirective
+   */
+  private initTemplateValue() {
+    let inputValue = this.inputRef.value.toString();
+    inputValue = inputValue.replace(REG_EX_PATTERNS.ALLOW_ONLY_NUMBERS_AND_DOT, '');
+    if (inputValue.indexOf('.') < 0) {
+      inputValue = parseFloat(inputValue)
+        .toFixed(this.decimalMask.noOfDecimalPoint)
+        .toString();
+    } else {
+      const [wholePart, decimalPart] = inputValue.split('.');
+      if (decimalPart.length > this.decimalMask.noOfDecimalPoint) {
+        inputValue = inputValue.slice(0, inputValue.indexOf('.') + 1 + this.decimalMask.noOfDecimalPoint);
+      } else {
+        inputValue = parseFloat(inputValue)
+          .toFixed(this.decimalMask.noOfDecimalPoint)
+          .toString();
+      }
+    }
+    this.inputRef.value = this.setDecimal(this.onlyNumbersString(inputValue));
+  }
+  /**
+   * Formatting inputString to number string
+   *
+   * @param {string} inputString
+   * @returns {string}
+   * @memberof InputDecimalMaskDirective
+   */
+  onlyNumbersString(inputString: string): string {
+    return inputString.replace(REG_EX_PATTERNS.ALLOW_ONLY_NUMBERS, '');
+  }
+  @HostListener('ionFocus', ['$event']) onfocus() {
     if (!this.inputRef.value) {
       this.inputRef.value = '0';
-      this.decModelChange.emit('0');
     }
   }
-  @HostListener('ionBlur', ['$event']) onBlur(e) {
-    const cleann = parseFloat(this.inputRef.value.toString().replace(/[^0-9]/g, ''));
-    if (cleann === 0) {
+  @HostListener('ionBlur', ['$event']) onBlur() {
+    if (parseFloat(this.onlyNumbersString(this.inputRef.value.toString())) === 0) {
       this.inputRef.value = '';
-      this.decModelChange.emit('');
-    }
-
-    if (this.inputRef.value.toString().length > this.maxDigit) {
-      this.decModelChange.emit(this.inputRef.value.toString().slice(0, this.maxDigit));
     }
   }
 
-  @HostListener('keyup', ['$event']) onChange(e) {
-    if (e.target.value) {
-      if (e.target.value.length <= this.maxDigit) {
-        let clean: any = e.target.value.replace(/[^0-9]/g, '');
-
-        if (clean.length < this.noOfDecimal + 2) {
-          clean = '0' + clean;
+  @HostListener('ionChange', ['$event']) onIonChange(event): void {
+    const onChangeInputValue = event.target.value;
+    if (onChangeInputValue) {
+      if (onChangeInputValue.length <= this.decimalMask.maxDigit) {
+        let onlyNumbersString = this.onlyNumbersString(onChangeInputValue);
+        if (onlyNumbersString.length < this.decimalMask.noOfDecimalPoint + 2) {
+          onlyNumbersString = '0' + onlyNumbersString;
         }
 
-        if (clean[0] === '0' && clean.length > this.noOfDecimal + 1) {
-          clean = clean.substr(1);
+        if (onlyNumbersString[0] === '0' && onlyNumbersString.length > this.decimalMask.noOfDecimalPoint + 1) {
+          onlyNumbersString = onlyNumbersString.substr(1);
         }
-
-        this.inputRef.value = this.setDecimal(clean, this.noOfDecimal);
-        const a = this.inputRef.value;
-        this.decModelChange.emit(a.replace(/[^0-9.]/g, ''));
+        this.inputRef.value = this.setDecimal(onlyNumbersString);
       } else {
-        this.inputRef.value = this.inputRef.value.toString().slice(0, this.maxDigit);
+        this.inputRef.value = this.inputRef.value.toString().slice(0, this.decimalMask.maxDigit);
       }
     }
   }
 
-  @HostListener('keypress', ['$event']) onInputChange(e) {
-    if (e.target.value.length >= this.maxDigit) {
-      e.preventDefault();
-    }
+  /**
+   * Return two part defference
+   *
+   * @param {string} onlyNumbersString
+   * @returns {number}
+   * @memberof InputDecimalMaskDirective
+   */
+  wholePartLength(onlyNumbersString: string): number {
+    return onlyNumbersString.length - this.decimalMask.noOfDecimalPoint;
   }
 
-  @HostListener('keydown', ['$event']) onInputChangekeydown(e) {
-    if (e.target.value.length > this.maxDigit) {
-      e.preventDefault();
-    }
-  }
-
-  setDecimal(value, decimalPlace) {
-    let int = value.substr(0, value.length - decimalPlace);
-
-    /*----------  Handling Comma  ----------*/
-    int = int
+  /**
+   * This function taking number string and formate with our define pattern
+   *
+   * @param {string} onlyNumbersString
+   * @returns {string}
+   * @memberof InputDecimalMaskDirective
+   */
+  setDecimal(onlyNumbersString: string): string {
+    let wholeNumberPart = onlyNumbersString.substr(0, this.wholePartLength(onlyNumbersString));
+    const decimalPart = onlyNumbersString.substr(this.wholePartLength(onlyNumbersString));
+    wholeNumberPart = wholeNumberPart
       .split('')
       .reverse()
       .join('');
-    const formatted = int.replace(/^(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,3})*/, '$1,$2,$3,$4,$5,$6');
-    int = formatted
+    wholeNumberPart = wholeNumberPart
+      .replace(REG_EX_PATTERNS.MONEY_WITH_COMMA, '$1,$2,$3')
       .split('')
       .reverse()
       .join('');
-    int = int.replace(/(^,*)/g, '');
-    /*----------  END  ----------*/
+    wholeNumberPart = wholeNumberPart.replace(REG_EX_PATTERNS.NOT_ALLOW_COMMA, '');
 
-    const dec = value.substr(value.length - decimalPlace);
-    if (+decimalPlace > 0) {
-      return this.currency + int + '.' + dec;
+    if (+this.decimalMask.noOfDecimalPoint > 0) {
+      return this.decimalMask.symbol + wholeNumberPart + '.' + decimalPart;
     } else {
-      return this.currency + int;
+      return this.decimalMask.symbol + wholeNumberPart;
     }
   }
 }
