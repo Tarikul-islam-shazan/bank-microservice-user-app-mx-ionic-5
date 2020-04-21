d8831a56bc2659cf5478f5be7704f141a02dc72c
Zahidul Islam <zahidul@bs-23.net>
2020-04-21 18:57:37 +0600

    es translation, analytics added

--- src/app/modules/signup/general-information/facade/general-information.facade.ts
+++ src/app/modules/signup/general-information/facade/general-information.facade.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@angular/core';
 import { SignUpService, IGeneralInfo, JumioApiService, IScannedIdData } from '@app/core';
 import { Router } from '@angular/router';
 import * as moment from 'moment';
+import { AnalyticsService, AnalyticsEventTypes } from '@app/analytics';
 
 @Injectable()
 export class GeneralInformationFacade {
   public maxDate: string;
-  constructor(private signUpService: SignUpService, private router: Router, private jumioService: JumioApiService) {
+  constructor(
+    private signUpService: SignUpService,
+    private router: Router,
+    private jumioService: JumioApiService,
+    private analytics: AnalyticsService
+  ) {
     this.maxDate = moment()
       .subtract(18, 'year')
       .format('YYYY-MM-DD');
@@ -14,6 +20,7 @@ export class GeneralInformationFacade {
 
   applyGeneralInformation(generalInformation: IGeneralInfo) {
     this.signUpService.applyGeneralInformation(generalInformation).subscribe(resp => {
+      this.analytics.logEvent(AnalyticsEventTypes.GeneralInfoSubmitted);
       this.router.navigateByUrl('/signup/living-information');
     });
   }
