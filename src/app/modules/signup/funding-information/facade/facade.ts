20c047eeb92e7cd1cf17d920d1b7c744903b166d
Tarikul Islam <kmtarikulislamshazan14062@gmail.com>
2020-04-29 15:25:37 +0600

    firebase analytics add on funding information

--- src/app/modules/signup/funding-information/facade/facade.ts
+++ src/app/modules/signup/funding-information/facade/facade.ts
@@ -9,13 +9,14 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { SignUpService } from '@app/core';
 import { IFundInfo } from '../model/fundinfo';
+import { AnalyticsService, AnalyticsEventTypes } from '@app/analytics';
 import * as moment from 'moment';
 
 @Injectable()
 export class FundingInformationFacade {
   fundInfo: IFundInfo;
 
-  constructor(private signupService: SignUpService, private router: Router) {}
+  constructor(private signupService: SignUpService, private router: Router, private analytics: AnalyticsService) {}
 
   /**
    * @method fundInformationSubmit Initialization funding information form
@@ -29,12 +30,16 @@ export class FundingInformationFacade {
 
   fundInformationSubmit(fundInformation) {
     this.fundInfo = fundInformation;
+    let fundtype = '';
     if (this.fundInfo.fundMyself) {
+      fundtype = 'Self';
       delete this.fundInfo.providerInfo;
     } else {
+      fundtype = 'Someone else';
       this.fundInfo.providerInfo.dateOfBirth = moment(this.fundInfo.providerInfo.dateOfBirth).format('MM-DD-YYYY');
     }
     this.signupService.fundingInformationSubmission(this.fundInfo).subscribe(res => {
+      this.analytics.logEvent(AnalyticsEventTypes.FundingProviderSelect, { whoWillFund: fundtype });
       this.router.navigate(['/signup/government-disclosure']);
     });
   }
