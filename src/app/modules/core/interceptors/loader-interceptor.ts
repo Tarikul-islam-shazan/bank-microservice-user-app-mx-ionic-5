3ea5b9c81b82ae8de10921ac38320f193079091e
Rahadur Rahman <rahadur.rahman@brainstation23.com>
2020-05-05 16:34:43 +0600

    Typing interrupted by 'Please wait' pop up.

--- src/app/modules/core/interceptors/loader-interceptor.ts
+++ src/app/modules/core/interceptors/loader-interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable, from } from 'rxjs';
-import { finalize, tap, switchMap } from 'rxjs/operators';
+import { finalize, switchMap } from 'rxjs/operators';
 import { LoadingService } from '../services/loading.service';
 import { Logger } from '../services/logger.service';
 
@@ -12,16 +12,32 @@ const log = new Logger('LoadingInterceptor');
 export class LoaderInterceptor implements HttpInterceptor {
   constructor(public loadingService: LoadingService) {}
 
+  /**
+   * This Interceptor responsible for showing loading for each httprequest
+   * But this loading not showing when we get extra header skip-loader within a headers
+   * Issue: MM2-237
+   * Details: typing interrupted by "Please wait" pop up.
+   * @param {HttpRequest<any>} req
+   * @param {HttpHandler} next
+   * @returns {Observable<HttpEvent<any>>}
+   * @memberof LoaderInterceptor
+   */
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     log.info('start');
-    return from(this.loadingService.show({ loadingMessage: 'Please wait...' })).pipe(
-      switchMap((loading: HTMLIonLoadingElement) => {
-        return next.handle(req).pipe(
-          finalize(() => {
-            this.loadingService.dismiss();
-          })
-        );
-      })
-    );
+    if (req.headers.has('skip-loader')) {
+      const newHeaders = req.headers.delete('skip-loader');
+      const newRequest = req.clone({ headers: newHeaders });
+      return next.handle(newRequest);
+    } else {
+      return from(this.loadingService.show({ loadingMessage: 'Please wait...' })).pipe(
+        switchMap((loading: HTMLIonLoadingElement) => {
+          return next.handle(req).pipe(
+            finalize(() => {
+              this.loadingService.dismiss();
+            })
+          );
+        })
+      );
+    }
   }
 }
