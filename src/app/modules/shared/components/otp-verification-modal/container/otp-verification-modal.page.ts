7d7cea189121c7730029e1c8d40f1dda5e1a7b4a
raihan zaman <raihanuzzaman@bs-23.net>
2020-05-11 15:21:54 +0600

    MM2-209 OTP screen UI update.

--- src/app/modules/shared/components/otp-verification-modal/container/otp-verification-modal.page.ts
+++ src/app/modules/shared/components/otp-verification-modal/container/otp-verification-modal.page.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonContent, ModalController, IonInput } from '@ionic/angular';
-import { OtpVerificationModalFacade } from '../facade';
+import { OtpVerificationModalFacade, OtpResponseType } from '../facade';
+import { REG_EX_PATTERNS } from '@app/core/models/patterns';
 import { IinputOption, InputFormatType } from '@app/shared/directives/mask-input.directive';
 @Component({
   selector: 'mbc-otp-verification-modal',
@@ -8,7 +9,6 @@ import { IinputOption, InputFormatType } from '@app/shared/directives/mask-input
   styleUrls: ['./otp-verification-modal.page.scss']
 })
 export class OtpVerificationModalPage {
-  // @ViewChild(IonContent, { static: false }) content: IonContent;
   // Reference of OTP input field
 
   codeError = false;
@@ -16,7 +16,6 @@ export class OtpVerificationModalPage {
 
   @ViewChild('hiddenInput', { static: false }) hiddenInput: IonInput;
   @ViewChild(IonContent, { static: false }) content: IonContent;
-  codeRegex = /^-?([0-9]\d*)?$/;
   verificationCode: string;
   emailVerification: IinputOption;
   /**
@@ -70,20 +69,23 @@ export class OtpVerificationModalPage {
     }
     if (resp && resp.status === 403) {
       const responseBody = resp.error.data;
-      if (responseBody.code === '704') {
+      if (responseBody.code === OtpResponseType.invalid) {
+        // invalid token
         this.codeError = true;
         setTimeout(() => {
           this.codeError = false;
           this.verificationCode = '';
         }, 1500);
       }
-      if (responseBody.code === '705') {
+      if (responseBody.code === OtpResponseType.tooManyAttempts) {
+        // too many attempts
         this.isInput = false;
       }
     }
   }
 
   dismiss(resp?: any) {
+    // services are not called in the component, that's why modal service is not being used here
     this.modalCtrl.dismiss(resp);
   }
 
@@ -94,7 +96,7 @@ export class OtpVerificationModalPage {
   }
 
   checkVerificationCodeValid() {
-    if (this.verificationCode.length === 6 && this.codeRegex.test(this.verificationCode)) {
+    if (this.verificationCode.length === 6 && REG_EX_PATTERNS.OTP_VALIDATION.test(this.verificationCode)) {
       return true;
     }
     return false;
--- src/app/modules/shared/components/otp-verification-modal/facade/index.ts
+++ src/app/modules/shared/components/otp-verification-modal/facade/index.ts
@@ -1,3 +1,8 @@
 import { OtpVerificationModalFacade } from './otp-verification-modal.facade';
 export const FACADE_SERVICE = [OtpVerificationModalFacade];
 export * from './otp-verification-modal.facade';
+
+export enum OtpResponseType {
+  invalid = '704',
+  tooManyAttempts = '705'
+}
