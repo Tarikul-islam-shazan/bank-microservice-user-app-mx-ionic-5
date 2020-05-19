21e2c8d8e4efd6cbfab4df06ec9f19d1fc4961ee
raihan zaman <raihanuzzaman@bs-23.net>
2020-05-19 11:42:10 +0600

    MM2-311 Atm finder: Location permission is not working properly.

--- src/app/modules/more/atm-finder/container/atm-finder.page.ts
+++ src/app/modules/more/atm-finder/container/atm-finder.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AtmFinderFacade } from '../facade';
 
 @Component({
@@ -6,16 +6,28 @@ import { AtmFinderFacade } from '../facade';
   templateUrl: './atm-finder.page.html',
   styleUrls: ['./atm-finder.page.scss']
 })
-export class AtmFinderPage {
+export class AtmFinderPage implements OnInit {
   selectedTab = 'map';
 
   constructor(public facade: AtmFinderFacade) {}
+
+  async ngOnInit() {
+    await this.facade.checkLocationResume();
+  }
 
   ionViewWillEnter() {
     this.facade.getNearestAtms();
   }
 
+  ionViewDidEnter() {
+    this.facade.viewDidEnter();
+  }
+
   switchTab(tabName: string) {
     this.selectedTab = tabName;
+  }
+
+  ionViewWillLeave() {
+    this.facade.willLeave();
   }
 }
