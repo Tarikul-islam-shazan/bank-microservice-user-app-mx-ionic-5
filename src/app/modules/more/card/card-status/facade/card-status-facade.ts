5a186974282db144f946e91650da994975dce453
Abdul Baten <md.abdul@brainstation23.com>
2020-06-02 14:36:40 +0600

    Feat/MM2-96 - Card: lock/unlock card

--- src/app/modules/more/card/card-status/facade/card-status-facade.ts
+++ src/app/modules/more/card/card-status/facade/card-status-facade.ts
@@ -3,41 +3,59 @@ import { CardService, ICard, ICardParams, CardStatus } from '@app/core';
 import { Observable } from 'rxjs';
 import { NavController } from '@ionic/angular';
 import { AnalyticsService, AnalyticsEventTypes } from '@app/analytics';
-import { tap, map, take } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Injectable()
 export class CardStatusFacade {
-  cards$: Observable<ICard[]>;
+  cards$: Observable<ICard>;
   constructor(private cardService: CardService, private navCtl: NavController, private analytics: AnalyticsService) {
     this.getCardDetails();
   }
 
+  /**
+   * @summary gets card details
+   *
+   * @returns {void}
+   * @memberOf CardStatusFacade
+   */
   getCardDetails(): void {
     this.cards$ = this.cardService.cardDetails;
   }
 
+  /**
+   * @summary changes card activate status
+   *
+   * @private
+   * @param {ICardParams} apiData
+   * @returns {Observable<ICard>}
+   * @memberOf CardStatusFacade
+   */
+  private changeCardActiveStatus(apiData: ICardParams): Observable<ICard> {
+    return this.cardService.cards$.pipe(
+      tap((card: ICard) => {
+        if (card.cardId === apiData.cardId) {
+          card.isCardActive = apiData.state === CardStatus.Locked ? false : true;
+        }
+      })
+    );
+  }
+
+  /**
+   * @summary updates card status
+   *
+   * @param {ICardParams} apiData
+   * @returns {void}
+   * @memberOf CardStatusFacade
+   */
   updateCardStatus(apiData: ICardParams): void {
     this.analytics.logEvent(AnalyticsEventTypes.CardStatusChanged);
     this.cardService
       .updateCardStatus(apiData)
       .pipe(
-        tap(resp => {
-          this.cardService.cards$
-            .pipe(
-              map((cardsObj: any) => {
-                const { cards } = cardsObj;
-                return cards.forEach((c: ICard) => {
-                  if (c.cardId === apiData.cardId) {
-                    c.isCardActive = apiData.state === CardStatus.Locked ? false : true;
-                  }
-                });
-              }),
-              take(1)
-            )
-            .subscribe();
+        switchMap(() => {
+          return this.changeCardActiveStatus(apiData);
         })
       )
-      .subscribe();
-    this.navCtl.pop();
+      .subscribe(() => this.navCtl.pop());
   }
 }
