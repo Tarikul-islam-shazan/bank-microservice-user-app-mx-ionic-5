7751c7f90500803add987e35d48555662be26d3c
Zahidul Islam <zahidul@bs-23.net>
2020-06-12 14:12:42 +0600

    issue solved

--- src/app/modules/signup/general-information/container/general-information.page.ts
+++ src/app/modules/signup/general-information/container/general-information.page.ts
@@ -4,10 +4,10 @@ import { GeneralInformationFacade } from '../facade';
 import { IGeneralInfo } from '@app/core';
 import * as moment from 'moment';
 /**
- * * Ticket: MM2-3
- * * Issue Details: General Information API Integration
- * * Developer Feedback: API integrated
- * Date: April 21, 2020
+ * * Ticket: MM2-332
+ * * Issue Details: CURP Verification issue
+ * * Developer Feedback: Issue Fixed
+ * Date: Jun 12, 2020
  * Developer: Zahidul Islam <zahidul@bs-23.net>
  */
 @Component({
@@ -27,15 +27,46 @@ export class GeneralInformationPage implements OnInit {
 
   initGeneralInfoForm() {
     this.generalForm = this.formBuilder.group({
-      firstName: ['', [Validators.required, Validators.maxLength(26)]],
-      secondName: ['', [Validators.maxLength(26)]],
-      paternalLastName: ['', [Validators.required, Validators.maxLength(26)]],
-      maternalLastName: ['', [Validators.maxLength(26)]],
+      firstName: ['', [Validators.required, Validators.maxLength(26), Validators.pattern('^[A-Za-z]+$')]],
+      secondName: ['', [Validators.maxLength(26), Validators.pattern('^[A-Za-z]+$')]],
+      paternalLastName: ['', [Validators.required, Validators.maxLength(26), Validators.pattern('^[A-Za-z]+$')]],
+      maternalLastName: ['', [Validators.maxLength(26), Validators.pattern('^[A-Za-z]+$')]],
       dateOfBirth: ['', [Validators.required]],
-      curp: ['', [Validators.required, Validators.minLength(18), Validators.maxLength(18)]],
+      curp: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(18),
+          Validators.maxLength(18),
+          Validators.pattern('[A-Z]{4}\\d{6}[HM]{1}[A-Z]{5}[A-Z0-9]{1}\\d{1}')
+        ]
+      ],
       mobileNumber: ['', [Validators.required, Validators.pattern('\\d{10}')]]
     });
     this.initJumioDataIntoForm();
+  }
+
+  addCurpValidators() {
+    const year = new Date(this.generalForm.controls.dateOfBirth.value).getFullYear();
+    this.generalForm.controls.curp.clearValidators();
+    if (year >= 2000) {
+      this.generalForm.controls.curp.setValidators([
+        Validators.required,
+        Validators.minLength(18),
+        Validators.maxLength(18),
+        Validators.pattern('[A-Z]{4}\\d{6}[HM]{1}[A-Z]{5}[A-Z]{1}\\d{1}')
+      ]);
+    } else {
+      this.generalForm.controls.curp.setValidators([
+        Validators.required,
+        Validators.minLength(18),
+        Validators.maxLength(18),
+        Validators.pattern('[A-Z]{4}\\d{6}[HM]{1}[A-Z]{5}[0-9]{1}\\d{1}')
+      ]);
+    }
+    if (this.generalForm.controls.curp.value) {
+      this.generalForm.controls.curp.patchValue(this.generalForm.controls.curp.value);
+    }
   }
 
   initJumioDataIntoForm() {
