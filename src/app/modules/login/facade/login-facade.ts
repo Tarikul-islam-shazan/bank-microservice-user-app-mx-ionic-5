1f95758bd04643f6ef49f979e1fd34f634a43181
Abdul Baten <md.abdul@brainstation23.com>
2020-06-26 16:17:17 +0600

    Fix/MM2-458 - Change login screen ui according to latest mock desing

--- src/app/modules/login/facade/login-facade.ts
+++ src/app/modules/login/facade/login-facade.ts
@@ -15,6 +15,8 @@ import { AppPlatform } from '@app/core/util/app-platform';
 import { ModalService, IMeedModalComponentProps } from '@app/shared';
 import { CallNumber } from '@ionic-native/call-number/ngx';
 import { StaticDataService } from '@app/core/services/static-data.service';
+import { ActionSheetController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 @Injectable()
 export class LoginFacade {
   constructor(
@@ -31,7 +33,9 @@ export class LoginFacade {
     private appPlatform: AppPlatform,
     private modalService: ModalService,
     private callService: CallNumber,
-    private staticDataService: StaticDataService
+    private staticDataService: StaticDataService,
+    private actionSheetController: ActionSheetController,
+    private translate: TranslateService
   ) {}
 
   /**
@@ -76,6 +80,7 @@ export class LoginFacade {
   get username(): string {
     return this.settingsService.getSettings().userSettings.username;
   }
+
   get rememberUsername(): boolean {
     const userSettings = this.settingsService.getSettings().userSettings;
     return userSettings.rememberUsername ? userSettings.rememberUsername : false;
@@ -320,45 +325,50 @@ export class LoginFacade {
   }
 
   /**
-   * Fix GMA-4757
-   * This function opens help modal.
-   * @memberof LoginFacade
+   * @summary returns buttons for help action sheet
+   *
+   * @returns {Record<string, any>[]}
+   * @memberOf LoginFacade
    */
-  async openHelpModal(): Promise<void> {
-    let contactNumber = this.bankContactNumber;
-    const modalComponentContent: IMeedModalComponentProps = {
-      componentProps: {
-        contents: [
-          {
-            details: ['login-module.create-login-page.help-modal-message']
-          }
-        ],
-        actionButtons: [
-          {
-            text: 'login-module.create-login-page.help-modal-call-no',
-            params: { contactNumber },
-            cssClass: 'white-button',
-            handler: () => {
-              contactNumber = contactNumber.replace(/\s/g, '');
-              if (contactNumber) {
-                if (this.appPlatform.isIos || this.appPlatform.isAndroid) {
-                  this.callService.callNumber(contactNumber, true);
-                } else {
-                  window.open(contactNumber, '_system');
-                }
-              }
+  loginHelpActionButtons(): Record<string, any>[] {
+    const contactNumber = this.bankContactNumber.replace(/\s/g, '');
+    const buttons = [
+      {
+        text: this.translate.instant('login-module.create-login-page.help-modal-call-no'),
+        handler: () => {
+          if (contactNumber) {
+            if (this.appPlatform.isIos || this.appPlatform.isAndroid) {
+              this.callService.callNumber(contactNumber, true);
+            } else {
+              window.open(contactNumber, '_system');
             }
-          },
-          {
-            text: 'login-module.create-login-page.help-modal-cancle-btn-text',
-            cssClass: 'grey-outline-button',
-            handler: () => this.modalService.close()
           }
-        ]
+        }
+      },
+      {
+        text: this.translate.instant('login-module.create-login-page.help-modal-cancle-btn-text'),
+        role: 'cancel'
       }
-    };
-    await this.modalService.openInfoModalComponent(modalComponentContent);
+    ];
+
+    return buttons;
   }
+
+  /**
+   * @summary opens help action sheet
+   *
+   * @returns {Promise<void>}
+   * @memberOf LoginFacade
+   */
+  async openHelpActionSheet(): Promise<void> {
+    const actionSheet = await this.actionSheetController.create({
+      buttons: this.loginHelpActionButtons(),
+      cssClass: 'login-help-action-sheet',
+      mode: 'ios'
+    });
+    await actionSheet.present();
+  }
+
   get member(): IMember {
     return this.memberService.getCachedMember();
   }
