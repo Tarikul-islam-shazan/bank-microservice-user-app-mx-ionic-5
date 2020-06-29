6a98c0e16ba2488bac93696548eb61f1ee000d4f
Raihan-brain <raihanuzzaman@bs-23.net>
2020-06-29 21:47:53 +0600

    working on the design

--- src/app/modules/more/personal-details/change-address/utility-upload/container/utility-upload.page.ts
+++ src/app/modules/more/personal-details/change-address/utility-upload/container/utility-upload.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mbc-utility-upload',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./utility-upload.page.scss']
 })
 export class UtilityUploadPage implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  dismiss() {
+    this.router.navigate([`/more/personal-details/change-address`]);
+  }
 }
