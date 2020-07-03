405a55c1d7816c595e5a7492721705cbf4f56933
Sudipta <sudipta.ghosh@bs-23.net>
2020-07-03 16:54:09 +0600

    fix/MM2-515: MeedTravel: Meed travel SSO integrated for web

--- src/app/modules/meed-travel/meed-trave.module.ts
+++ src/app/modules/meed-travel/meed-trave.module.ts
@@ -3,10 +3,11 @@ import { MeedTravelPageRoutingModule } from './meed-travel-routing.module';
 import { MeedTravelPage } from './container/meed-travel.page';
 import { MEED_TRAVEL_FACADE_SERVICE } from './facade';
 import { SharedModule } from '@app/shared';
+import { WebsitePage } from './third-party-website/container/third-party-website.page';
 
 @NgModule({
   imports: [SharedModule, MeedTravelPageRoutingModule],
-  declarations: [MeedTravelPage],
+  declarations: [MeedTravelPage, WebsitePage],
   providers: [...MEED_TRAVEL_FACADE_SERVICE]
 })
 export class MeedTravelPageModule {}
