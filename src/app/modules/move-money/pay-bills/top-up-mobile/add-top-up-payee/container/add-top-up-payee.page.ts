a238121deb2c9e5095359ce532c7c28d8c74bc2a
Md.Kausar <md.kausar@brainstation23.com>
2020-07-28 17:23:55 +0600

    MM2-601: Remove unused code

--- src/app/modules/move-money/pay-bills/top-up-mobile/add-top-up-payee/container/add-top-up-payee.page.ts
+++ src/app/modules/move-money/pay-bills/top-up-mobile/add-top-up-payee/container/add-top-up-payee.page.ts
@@ -2,7 +2,6 @@ import { AddTopUpPayeeFacade } from '../facade';
 import { CommonValidators, IBiller } from '@app/core';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { IinputOption, InputFormatType } from '@app/shared';
 
 @Component({
   selector: 'add-top-up-payee',
@@ -12,7 +11,6 @@ import { IinputOption, InputFormatType } from '@app/shared';
 export class AddTopUpPayeePage implements OnInit {
   addPayeeForm: FormGroup;
   biller: IBiller;
-  phoneNumber: IinputOption;
   constructor(private facade: AddTopUpPayeeFacade, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
@@ -28,9 +26,6 @@ export class AddTopUpPayeePage implements OnInit {
    * @memberOf AddPayeePage
    */
   private initForm(): void {
-    this.phoneNumber = {
-      type: InputFormatType.PHONE_MASK
-    };
     this.addPayeeForm = this.formBuilder.group(
       {
         name: [{ value: this.biller.name, disabled: true }, [Validators.required]],
