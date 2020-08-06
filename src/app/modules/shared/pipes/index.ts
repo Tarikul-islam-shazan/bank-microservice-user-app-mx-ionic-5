d39a53e60677baa9d30318c478b825d7614214ba
Md.Kausar <md.kausar@brainstation23.com>
2020-08-06 16:24:04 +0600

    MM2-629:Phone pipe is  added to shared module

--- src/app/modules/shared/pipes/index.ts
+++ src/app/modules/shared/pipes/index.ts
@@ -1,9 +1,10 @@
 import { NegativeToPositivePipe } from './negative-to-positive.pipe';
 import { EmailNamePipe } from './email-name.pipe';
 import { LocaleDatePipe } from './locale-date.pipe';
-
-export const SHARED_PIPES: any[] = [NegativeToPositivePipe, EmailNamePipe, LocaleDatePipe];
+import { PhonePipe } from './phone.pipe';
+export const SHARED_PIPES: any[] = [NegativeToPositivePipe, EmailNamePipe, LocaleDatePipe, PhonePipe];
 
 export * from './negative-to-positive.pipe';
 export * from './email-name.pipe';
 export * from './locale-date.pipe';
+export * from './phone.pipe';
