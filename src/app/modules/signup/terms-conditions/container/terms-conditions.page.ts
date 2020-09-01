806f24ebcd3a575899fac11425c43f07cfdcd878
Zahidul Islam <zahidul@bs-23.net>
2020-09-01 18:40:44 +0600

    issue solved

--- src/app/modules/signup/terms-conditions/container/terms-conditions.page.ts
+++ src/app/modules/signup/terms-conditions/container/terms-conditions.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { TncResponse } from '@app/core/models/dto/signup';
+import { TncDocument } from '@app/core';
 import { SignUpTermsConditionFacade } from '../facade';
-import { FormGroup, FormBuilder, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-terms-conditions',
@@ -9,93 +9,36 @@ import { FormGroup, FormBuilder, FormControl, Validators, FormArray } from '@ang
   styleUrls: ['./terms-conditions.page.scss']
 })
 export class TermsConditionsPage implements OnInit {
-  tncResponse: Partial<TncResponse> = {};
+  termsConditions: TncDocument[] = [];
   termsConditionForm: FormGroup;
-  corporateTnCAccepted = false;
-  corporateTnCNotAccepted = false;
   constructor(private formBuilder: FormBuilder, public facade: SignUpTermsConditionFacade) {}
 
   ngOnInit() {
+    this.getTermsConditions();
     this.initForm();
   }
   initForm() {
     this.termsConditionForm = this.formBuilder.group({});
-    this.getTermsConditions();
   }
-
   async showPdf(code: string, title: string) {
     const doc = await this.facade.getTermsConditionBase64String(code);
     this.facade.openPdfViewer({ base64DataOrUrl: doc.document, pdfTitle: title });
   }
-
   getTermsConditions() {
     this.facade.getTermsConditions().subscribe(resp => {
-      this.tncResponse = { ...resp };
-      this.tncResponse.termsAndConditions.forEach((tc, i) => {
+      this.termsConditions = resp;
+      this.termsConditions.forEach((tc, i) => {
         this.termsConditionForm.addControl(
           `tc${i}`,
           new FormControl(false, [Validators.required, Validators.requiredTrue])
         );
       });
-      if (this.tncResponse.tncOptions.hasCorporateTnc) {
-        this.termsConditionForm.addControl('corporateTnCAccepted', new FormControl(false, Validators.required));
-        this.termsConditionForm.addControl('corporateTnCNotAccepted', new FormControl(false, Validators.required));
-      }
     });
   }
 
-  acceptTermsConditions(): void {
-    if (this.validateTNC()) {
-      this.facade.submitTermsCondition(
-        this.tncResponse.tncOptions.hasCorporateTnc,
-        this.corporateTnCAccepted,
-        this.tncResponse.tncOptions.nextPage
-      );
-    }
-  }
-  /**
-   * This method check or uncheck "ion-checkbox" for
-   * Ok to Share or Do Not Share checkbox.
-   *
-   * @param null
-   * @returns null
-   */
-  onCorporateTnCAccepted(event): void {
-    if (event.target.checked) {
-      this.corporateTnCAccepted = event.target.checked;
-      this.corporateTnCNotAccepted = !this.corporateTnCAccepted;
-      this.termsConditionForm.controls.corporateTnCAccepted.patchValue(this.corporateTnCAccepted);
-      this.termsConditionForm.controls.corporateTnCNotAccepted.patchValue(this.corporateTnCNotAccepted);
-    } else {
-      this.corporateTnCAccepted = event.target.checked;
-      this.termsConditionForm.controls.corporateTnCAccepted.patchValue(this.corporateTnCAccepted);
-    }
-  }
-
-  /**
-   * This method check or uncheck "ion-checkbox" for
-   * Ok to Share or Do Not Share checkbox.
-   *
-   * @param null
-   * @returns null
-   */
-  onCorporateTnCNotAccepted(event): void {
-    if (event.target.checked) {
-      this.corporateTnCNotAccepted = event.target.checked;
-      this.corporateTnCAccepted = !this.corporateTnCNotAccepted;
-      this.termsConditionForm.controls.corporateTnCAccepted.patchValue(this.corporateTnCAccepted);
-      this.termsConditionForm.controls.corporateTnCNotAccepted.patchValue(this.corporateTnCNotAccepted);
-    } else {
-      this.corporateTnCNotAccepted = event.target.checked;
-      this.termsConditionForm.controls.corporateTnCNotAccepted.patchValue(this.corporateTnCNotAccepted);
-    }
-  }
-
-  validateTNC(): boolean {
-    if (this.tncResponse.tncOptions !== undefined && this.tncResponse.tncOptions.hasCorporateTnc) {
-      return this.termsConditionForm.valid && (this.corporateTnCAccepted || this.corporateTnCNotAccepted);
-    } else {
-      return this.termsConditionForm.valid;
+  async acceptTermsConditions() {
+    if (this.termsConditionForm.valid) {
+      this.facade.acceptTermsCondition();
     }
   }
 }
--- src/app/modules/signup/terms-conditions/facade/facade.ts
+++ src/app/modules/signup/terms-conditions/facade/facade.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { SignUpService } from '@app/core/services/sign-up-service.service';
+import { SignUpService, SettingsService, TncDocument } from '@app/core';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
@@ -7,9 +7,6 @@ import { AccountApprovedModalComponent } from '@app/signup/components/account-ap
 import { AnalyticsService, AnalyticsEventTypes } from '@app/analytics';
 import { UserSettings } from '@app/core/models/app-settings';
 import { IPDFContent, PdfViewerService } from '@app/core/services/pdf-viewer.service';
-import { IMeedModalContent, ModalService } from '@app/shared/services/modal.service';
-import { TncResponse } from '@app/core/models/dto/signup';
-import { SettingsService } from '@app/core/services/settings.service';
 /**
  * * Issue: MM2-56
  * * Issue Details: Terms & Conditions - Screen
@@ -26,11 +23,10 @@ export class SignUpTermsConditionFacade {
     private modalCtrl: ModalController,
     private analytics: AnalyticsService,
     private settingsService: SettingsService,
-    private pdfService: PdfViewerService,
-    private modalService: ModalService
+    private pdfService: PdfViewerService
   ) {}
 
-  getTermsConditions(): Observable<TncResponse> {
+  getTermsConditions(): Observable<TncDocument[]> {
     return this.service.getTermsConditions();
   }
 
@@ -38,22 +34,14 @@ export class SignUpTermsConditionFacade {
     return this.service.getTermsConditionBase64String(code).toPromise();
   }
 
-  submitTermsCondition(hasCorporateTnc: boolean, corporateTnCAccepted: boolean, nextPage: string) {
-    if (hasCorporateTnc && !corporateTnCAccepted) {
-      this.corporateNotAcceptedAssuranceModal(corporateTnCAccepted, nextPage);
-    } else {
-      this.acceptTermsCondition(corporateTnCAccepted, nextPage);
-    }
-  }
-
-  acceptTermsCondition(corporateTnCAccepted: boolean, nextPage: string) {
-    this.service.acceptTermsCondition(corporateTnCAccepted).subscribe(resp => {
+  acceptTermsCondition() {
+    this.service.acceptTermsCondition().subscribe(resp => {
       this.analytics.logEvent(AnalyticsEventTypes.TermsAndConditionsAgreed);
-      this.openApprovedModal(corporateTnCAccepted, nextPage);
+      this.openApprovedModal();
     });
   }
 
-  async openApprovedModal(corporateTnCAccepted: boolean, nextPage: string) {
+  async openApprovedModal() {
     const userSettings: UserSettings = this.settingsService.getSettings().userSettings;
     userSettings.disabledSignUp = true;
     this.settingsService.setUserSettings(userSettings);
@@ -62,50 +50,10 @@ export class SignUpTermsConditionFacade {
     });
     modal.present();
     await modal.onDidDismiss();
-
-    if (corporateTnCAccepted) {
-      if (nextPage) {
-        this.router.navigate([`/signup/${nextPage}`]);
-      } else {
-        this.router.navigate(['/signup/account-funding']);
-      }
-    } else {
-      this.router.navigate(['/signup/account-funding']);
-    }
+    this.router.navigate(['/signup/deposit/direct-deposit-start']);
   }
 
   openPdfViewer(pdfContent: IPDFContent): void {
     this.pdfService.openPDFFromBase64Data(pdfContent);
-  }
-
-  async corporateNotAcceptedAssuranceModal(corporateTnCAccepted: boolean, nextPage: string): Promise<void> {
-    const componentProps: IMeedModalContent = {
-      contents: [
-        {
-          details: [
-            'signup-module.terms-conditions-page.corporate-terms-not-accepted-assurance-modal.corporate-terms-not-accepted-modal-text-1',
-            'signup-module.terms-conditions-page.corporate-terms-not-accepted-assurance-modal.corporate-terms-not-accepted-modal-text-2'
-          ]
-        }
-      ],
-      actionButtons: [
-        {
-          text: 'signup-module.terms-conditions-page.corporate-terms-not-accepted-assurance-modal.okay-button',
-          cssClass: 'white-button',
-          handler: () => {
-            this.modalService.close();
-            this.acceptTermsCondition(corporateTnCAccepted, nextPage);
-          }
-        },
-        {
-          text: 'signup-module.terms-conditions-page.corporate-terms-not-accepted-assurance-modal.cancel-button',
-          cssClass: 'cancel-button',
-          handler: () => {
-            this.modalService.close();
-          }
-        }
-      ]
-    };
-    await this.modalService.openInfoModalComponent({ componentProps });
   }
 }
